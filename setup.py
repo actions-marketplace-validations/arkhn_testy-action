8540c5cfce905603cc437e2a1fd5324c879cac32
Valentin Matton <taq@protonmail.ch>
2020-11-03 14:02:07 +0100

    [python] Add ansible runner dependency

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email="valentin@arkhn.com",
     description="GitHub Action to run Arkhn's integration test suite",
     long_description=readme,
-    install_requires=["requests>=2.24,<3"],
+    install_requires=["requests>=2.24,<3", "ansible-runner>=1.4,<2"],
     extras_require=extras_require,
     python_requires=">=3.8.*, <4",
     entry_points={"console_scripts": ["testy-action=arkhn.testy_action.main:main"]},
