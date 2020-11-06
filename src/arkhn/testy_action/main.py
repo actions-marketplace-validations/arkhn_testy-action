e11d9cee76ef402fe608b8ae406570e77cf77571
Valentin Matton <taq@protonmail.ch>
2020-11-06 11:00:43 +0100

    [package] Cleanup main

--- src/arkhn/testy_action/main.py
+++ src/arkhn/testy_action/main.py
@@ -3,7 +3,7 @@ import argparse
 from arkhn.testy_action.scaleway import APIClient
 
 
-def main():
+def build_args_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         prog="testy-action",
         description="Run Arkhn's integration test suites on cloud platform.",
@@ -22,24 +22,39 @@ def main():
         help="Project ID on the cloud platform",
     )
 
+    return parser
+
+
+def provision_server(api: APIClient) -> dict:
+    ubuntu_image = api.find_image(name="Ubuntu 18.04 Bionic Beaver")
+    server = api.create_server(
+        name="testy", image_id=ubuntu_image["id"], project_id=args.project_id
+    )
+    api.poweron_server(server["id"])
+
+    return server
+
+
+def withdraw_server(server: dict, api: APIClient) -> None:
+    api.poweroff_server(server["id"])
+
+    import time
+
+    time.sleep(40)
+
+    api.delete_server(server["id"])
+
+
+def main():
+    parser = build_args_parser()
     args = parser.parse_args()
 
     api = APIClient(auth_token=args.token)
 
-    ubuntu_image = api.find_image(name="Ubuntu 18.04 Bionic Beaver")
-
-    testy_server = api.create_server(
-        name="testy", image_id=ubuntu_image["id"], project_id=args.project_id
-    )
-
-    api.poweron_server(testy_server["id"])
+    server = provision_server(api)
 
     import time
 
     time.sleep(20)
 
-    api.poweroff_server(testy_server["id"])
-
-    time.sleep(20)
-
-    api.delete_server(testy_server["id"])
+    withdraw_server(server, api)
