8dcf8b5e470644d8e5180aa95950072427214460
Valentin Matton <taq@protonmail.ch>
2020-11-09 08:30:32 +0100

    [action] Add deploy key

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,13 @@ set -e
 # Trace execution
 [[ "${DEBUG}" ]] && set -x
 
+mkdir ~/.ssh
+echo "${INPUT_DEPLOYMENTTOKEN}" > ~/.ssh/deploy_key
+cat > ~/.ssh/config << EOF
+Host github
+    Hostname github.com
+    IdentityFile ~/.ssh/deploy_key
+    IdentitiesOnly yes
+EOF
+
 testy-action "${INPUT_CONTEXTNAME:+--context-name $INPUT_CONTEXTNAME}" "${INPUT_CLOUDTOKEN}" "${INPUT_CLOUDPROJECTID}"
