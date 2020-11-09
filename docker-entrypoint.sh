8843f92a61f30a41795ef66dbbf93f6e8f177787
Valentin Matton <taq@protonmail.ch>
2020-11-09 15:46:07 +0100

    [action] Use args instead of ssh config
    * Otherwise strange behaviour on GA: known_hosts are ignored...

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,19 +11,10 @@ set -e
 # Clone private deployment repository (Arkhn's ansible playbooks).
 ####
 function clone_deployment() {
-  # Dedicated ssh config for a GitHub deploy key.
-  mkdir -p ~/.ssh/deployment
-  echo "${INPUT_DEPLOYMENTTOKEN}" | base64 -d > ~/.ssh/deployment/deploy_key
-  chmod 400 ~/.ssh/deployment/deploy_key
-  cat > ~/.ssh/deployment/config <<-EOF
-  Host github.com
-    Hostname github.com
-    IdentityFile ~/.ssh/deployment/deploy_key
-    IdentitiesOnly yes
-    UserKnownHostsFile ~/.ssh/deployment/known_hosts
-EOF
-  ssh-keyscan github.com > ~/.ssh/deployment/known_hosts
-  GIT_SSH_COMMAND="ssh -F ~/.ssh/deployment/config" git clone git@github.com:arkhn/deployment.git
+  echo "${INPUT_DEPLOYMENTTOKEN}" | base64 -d > deployment_deploy_key
+  chmod 400 deployment_deploy_key
+  ssh-keyscan github.com >> known_hosts
+  GIT_SSH_COMMAND="ssh -o IdentityFile=deployment_deploy_key -o IdentitiesOnly=yes -o UserKnownHostsFile=known_hosts" git clone git@github.com:arkhn/deployment.git
 }
 
 clone_deployment
