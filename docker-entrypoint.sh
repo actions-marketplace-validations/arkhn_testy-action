f4a770d2d1ce841217fdeb7167507a4df7bca224
Valentin Matton <taq@protonmail.ch>
2020-11-09 09:05:28 +0100

    [action] Clone deployment repo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,13 +6,21 @@ set -e
 # Trace execution
 [[ "${DEBUG}" ]] && set -x
 
-mkdir ~/.ssh
-echo "${INPUT_DEPLOYMENTTOKEN}" > ~/.ssh/deploy_key
-cat > ~/.ssh/config << EOF
-Host github
+function clone_deployment() {
+  mkdir -p ~/.ssh/deployment
+  echo "${INPUT_DEPLOYMENTTOKEN}" > ~/.ssh/deployment/deploy_key
+  chmod 400 ~/.ssh/deployment/deploy_key
+  cat > ~/.ssh/deployment/config <<-EOF
+  Host github.com
     Hostname github.com
-    IdentityFile ~/.ssh/deploy_key
+    IdentityFile ~/.ssh/deployment/deploy_key
     IdentitiesOnly yes
+    UserKnownHostsFile ~/.ssh/deployment/known_hosts
 EOF
+  ssh-keyscan github.com > ~/.ssh/deployment/known_hosts
+  GIT_SSH_COMMAND="ssh -F ~/.ssh/deployment/config" git clone git@github.com:arkhn/deployment.git
+}
+
+clone_deployment
 
 testy-action "${INPUT_CONTEXTNAME:+--context-name $INPUT_CONTEXTNAME}" "${INPUT_CLOUDTOKEN}" "${INPUT_CLOUDPROJECTID}"
